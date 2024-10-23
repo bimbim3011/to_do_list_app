d4bf7fbb08835d882b8a4841cb783917ac788f7b
bimbim3011 <vthanh3011@gmail.com>
2024-10-23 21:32:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'models/todo.dart';
+import 'services/todo_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:to_do_list_app/models/todo.dart';
+
+class TodoService {
+  static
+}
